baeb5cf04f527ad67e88595c640aaea88783c8fd
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-06 01:34:39 +0100

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include "io.h"
 #include "expressions.h"
+#include <iostream>
 int main()
 {
     // 1. Read TXT
@@ -14,10 +15,34 @@ int main()
     auto SA = createSAMatrix(d.nFloors);
     auto Sx  = multiplyscamat(fx, SA);
     auto Sy  = multiplyscamat(fy, SA);
-    auto M = buildMassMatrix(d);
+//    auto M = buildMassMatrix(d);
+std::vector<std::vector<double>> M(13, std::vector<double>(13, 0.0));
+
+    double diag[13] =
+    {
+        539.7588,
+        526.1904,
+        524.1307,
+        522.0564,
+        522.3585,
+        507.0437,
+        490.7714,
+        492.1757,
+        492.0478,
+        454.4963,
+        380.9595,
+        330.399,
+        6.271963
+    };
+
+    for(int i = 0; i < 13; i++)
+        M[i][i] = diag[i];
+
     auto Dx = multiplymatmat(Sx, M);
     auto Dy = multiplymatmat(Sy, M);
+    Matrix phi,LA;
+    eigenDecomposition(Dx, phi, LA);
     // 3. Write results
-    writeOutput("output.txt", delta, fx, fy, SA, Sx, Sy, M, Dx, Dy);
+    writeOutput("output.txt", delta, fx, fy, SA, Sx, Sy, M, Dx, Dy, phi, LA);
     return 0;
 }
