75e6372aba37b991f1bd432692d68d135c356aeb
hassanezakaria161631099728-svg <hassane.zakaria.161631099728@gmail.com>
2026-03-13 05:25:37 +0100

    Delete header/structs.h

--- header/structs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-struct BeamData {
-    double w,L,E,I; // deflection
-    int nFloors;    // stiffness Mass Dynamic matrices
-    double M1, M2, M3, h, EC, IX, IY;
-};
